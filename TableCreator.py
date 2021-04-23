d57f2d3ecb2b4065d36de447f2bba2ce6032da22
Naveen KS <naveenk@virtualmaze.co.in>
2021-04-23 18:33:32 +0530

    final commit

--- TableCreator.py
+++ TableCreator.py
@@ -7,15 +7,17 @@ class TableCreator:
         self.config = config
         self.mongo = mongo
         self.createTable()
+        self.rowCount = 0
 
     def createTable(self):
 
-        tabular_fields = ["IP", "Country", "Owner","Detected urls","UnDetected urls","Detected Downloaded Samples","UnDetected Downloaded Samples"]
+        tabular_fields = ["S.NO","IP", "Country", "Owner","Detected urls","UnDetected urls","Detected Downloaded Samples","UnDetected Downloaded Samples"]
         self.tabular_table = PrettyTable()
         self.tabular_table.field_names = tabular_fields 
 
     def insertRow(self,rowData):
-
+        self.rowCount += 1
+        rowData.insert(0,str(self.rowCount))
         self.tabular_table.add_row(rowData)
 
     def getHtmlTableData(self):
